816111ddee7a5d76df55eca23c6b1a95e1d489ef
Henrik Enggaard Hansen <henrik.enggaard@gmail.com>
2013-11-06 16:03:32 +0000

    Remove util from global namespace

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,6 @@
-require(["grammar", "util", "truthfunction", "LaTeX", "karnaugh", "view/truthtable", "view/karnaugh"],
- function(grammar,   util,   truthfunction,   LaTeX,   karnaugh,   truthtableview,    karnaughview){
+require(["grammar", "truthfunction", "LaTeX", "karnaugh", "view/truthtable", "view/karnaugh"],
+ function(grammar,   truthfunction,   LaTeX,   karnaugh,   truthtableview,    karnaughview){
 	window.parser = grammar;
-	window.util = util;
 	window.truthfunction = truthfunction;
 	window.LaTeX = LaTeX;
 	window.karnaugh = karnaugh;
